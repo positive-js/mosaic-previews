e7f894a0b2659cb8f28e3ba9da5d584c8a2884d3
lkramarov <lkramarov@ptsecurity.com>
2019-11-26 11:20:37 +0000

    95c3b1f added empty line

---
+++ pr352-95c3b1f/assets/stackblitz/mosaic-module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { McMomentDateModule } from '@ptsecurity/mosaic-moment-adapter';
+import { McButtonModule } from '@ptsecurity/mosaic/button';
+import { McCheckboxModule } from '@ptsecurity/mosaic/checkbox';
+import { McDatepickerModule } from '@ptsecurity/mosaic/datepicker';
+import { McFormFieldModule } from '@ptsecurity/mosaic/form-field';
+import { McIconModule } from '@ptsecurity/mosaic/icon';
+import { McInputModule } from '@ptsecurity/mosaic/input';
+import { McRadioModule } from '@ptsecurity/mosaic/radio';
+import { McSelectModule } from '@ptsecurity/mosaic/select';
+import { McTimepickerModule } from '@ptsecurity/mosaic/timepicker';
+
+
+@NgModule({
+    exports: [
+        McMomentDateModule,
+        McButtonModule,
+        McCheckboxModule,
+        McDatepickerModule,
+        McFormFieldModule,
+        McIconModule,
+        McInputModule,
+        McRadioModule,
+        McTimepickerModule,
+        McSelectModule
+    ]
+})
+export class DemoMosaicModule {
+}
