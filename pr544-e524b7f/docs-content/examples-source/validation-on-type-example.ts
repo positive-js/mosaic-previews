5ebfe18d4efee6101fb3c345037f72272cbfb098
l.s.kramarov <l.s.kramarov@gmail.com>
2020-12-03 16:19:59 +0000

    e524b7f fixed linter errors

---
+++ pr544-e524b7f/docs-content/examples-source/validation-on-type-example.ts
@@ -0,0 +1,38 @@
+import { Component, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+
+/**
+ * @title validation-on-type
+ */
+@Component({
+    selector: 'validation-on-type-example',
+    templateUrl: 'validation-on-type-example.html',
+    styleUrls: ['validation-on-type-example.css']
+})
+export class ValidationOnTypeExample {
+    checkOnFlyForm: FormGroup;
+
+    @ViewChild('tooltip', { static: false }) tooltip: any;
+
+    constructor() {
+        this.checkOnFlyForm = new FormGroup({
+            folderName: new FormControl('')
+        });
+    }
+
+    onInput(event) {
+        const regex = /^[\d\w]+$/g;
+
+        if (!regex.test(event.target.value)) {
+            event.target.value = event.target.value.replace(/[^\d\w]+/g, '');
+
+            if (!this.tooltip.isTooltipOpen) {
+                this.tooltip.show();
+
+                // tslint:disable-next-line:no-magic-numbers
+                setTimeout(() => this.tooltip.hide(), 3000);
+            }
+        }
+    }
+}
