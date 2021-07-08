2810e41efe7b02636895969fccb6ea95aa0af771
Oleg Pimenov <pimenov.o@gmail.com>
2021-07-08 09:48:54 +0000

    4440b117 chore: fixed style for icon

---
+++ pr645-4440b11/24-es5.js
@@ -0,0 +1,128 @@
+(function () {
+  function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+  (window["webpackJsonp"] = window["webpackJsonp"] || []).push([[24], {
+    /***/
+    "bo/Q":
+    /*!******************************************************************************************************************!*\
+      !*** ./node_modules/@ptsecurity/mosaic-examples/fesm2015/ptsecurity-mosaic-examples-mosaic-common-sites-menu.js ***!
+      \******************************************************************************************************************/
+
+    /*! exports provided: SitesMenuExamplesModule, SitesMenuOverviewExample */
+
+    /***/
+    function boQ(module, __webpack_exports__, __webpack_require__) {
+      "use strict";
+
+      __webpack_require__.r(__webpack_exports__);
+      /* harmony export (binding) */
+
+
+      __webpack_require__.d(__webpack_exports__, "SitesMenuExamplesModule", function () {
+        return SitesMenuExamplesModule;
+      });
+      /* harmony export (binding) */
+
+
+      __webpack_require__.d(__webpack_exports__, "SitesMenuOverviewExample", function () {
+        return SitesMenuOverviewExample;
+      });
+      /* harmony import */
+
+
+      var _angular_common__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(
+      /*! @angular/common */
+      "ofXK");
+      /* harmony import */
+
+
+      var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(
+      /*! @angular/core */
+      "fXoL");
+      /**
+       * @title Basic site menu
+       */
+
+
+      var SitesMenuOverviewExample = function SitesMenuOverviewExample() {
+        _classCallCheck(this, SitesMenuOverviewExample);
+      };
+
+      SitesMenuOverviewExample.ɵfac = function SitesMenuOverviewExample_Factory(t) {
+        return new (t || SitesMenuOverviewExample)();
+      };
+
+      SitesMenuOverviewExample.ɵcmp = _angular_core__WEBPACK_IMPORTED_MODULE_1__["ɵɵdefineComponent"]({
+        type: SitesMenuOverviewExample,
+        selectors: [["sites-menu-overview-example"]],
+        decls: 2,
+        vars: 0,
+        template: function SitesMenuOverviewExample_Template(rf, ctx) {
+          if (rf & 1) {
+            _angular_core__WEBPACK_IMPORTED_MODULE_1__["ɵɵelementStart"](0, "p");
+
+            _angular_core__WEBPACK_IMPORTED_MODULE_1__["ɵɵtext"](1, "Test sites-menu-overview-example");
+
+            _angular_core__WEBPACK_IMPORTED_MODULE_1__["ɵɵelementEnd"]();
+          }
+        },
+        styles: [""]
+      });
+
+      (function () {
+        (typeof ngDevMode === "undefined" || ngDevMode) && _angular_core__WEBPACK_IMPORTED_MODULE_1__["ɵsetClassMetadata"](SitesMenuOverviewExample, [{
+          type: _angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"],
+          args: [{
+            selector: 'sites-menu-overview-example',
+            templateUrl: 'sites-menu-overview-example.html',
+            styleUrls: ['sites-menu-overview-example.css']
+          }]
+        }], null, null);
+      })();
+
+      var EXAMPLES = [SitesMenuOverviewExample];
+
+      var SitesMenuExamplesModule = function SitesMenuExamplesModule() {
+        _classCallCheck(this, SitesMenuExamplesModule);
+      };
+
+      SitesMenuExamplesModule.ɵfac = function SitesMenuExamplesModule_Factory(t) {
+        return new (t || SitesMenuExamplesModule)();
+      };
+
+      SitesMenuExamplesModule.ɵmod = _angular_core__WEBPACK_IMPORTED_MODULE_1__["ɵɵdefineNgModule"]({
+        type: SitesMenuExamplesModule
+      });
+      SitesMenuExamplesModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_1__["ɵɵdefineInjector"]({
+        imports: [[_angular_common__WEBPACK_IMPORTED_MODULE_0__["CommonModule"]]]
+      });
+
+      (function () {
+        (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_1__["ɵɵsetNgModuleScope"](SitesMenuExamplesModule, {
+          declarations: [SitesMenuOverviewExample],
+          imports: [_angular_common__WEBPACK_IMPORTED_MODULE_0__["CommonModule"]],
+          exports: [SitesMenuOverviewExample]
+        });
+      })();
+
+      (function () {
+        (typeof ngDevMode === "undefined" || ngDevMode) && _angular_core__WEBPACK_IMPORTED_MODULE_1__["ɵsetClassMetadata"](SitesMenuExamplesModule, [{
+          type: _angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"],
+          args: [{
+            imports: [_angular_common__WEBPACK_IMPORTED_MODULE_0__["CommonModule"]],
+            declarations: EXAMPLES,
+            exports: EXAMPLES
+          }]
+        }], null, null);
+      })();
+      /**
+       * Generated bundle index. Do not edit.
+       */
+      //# sourceMappingURL=ptsecurity-mosaic-examples-mosaic-common-sites-menu.js.map
+
+      /***/
+
+    }
+  }]);
+})();
+//# sourceMappingURL=24-es5.js.map
