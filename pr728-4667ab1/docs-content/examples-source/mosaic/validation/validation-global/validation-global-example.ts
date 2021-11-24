aef432ed4f9c97fa724257c14ea4ccdb048f325e
agalaktionov <agalaktionov@ptsecurity.com>
2021-11-24 14:48:27 +0000

    4667ab17 Доработки

---
+++ pr728-4667ab1/docs-content/examples-source/mosaic/validation/validation-global/validation-global-example.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ThemePalette } from '@ptsecurity/mosaic/core';
+
+
+/**
+ * @title validation-global
+ */
+@Component({
+    selector: 'validation-global-example',
+    templateUrl: 'validation-global-example.html',
+    styleUrls: ['validation-global-example.css']
+})
+export class ValidationGlobalExample {
+    themePalette = ThemePalette;
+
+    globalErrorForm: FormGroup;
+    showServerErrors: boolean = false;
+    inProgress: boolean = false;
+
+    constructor() {
+        this.globalErrorForm = new FormGroup({
+            firstName: new FormControl(''),
+            lastName: new FormControl(''),
+            thirdName: new FormControl('')
+        });
+    }
+
+    submitGlobalErrorForm() {
+        this.showServerErrors = false;
+        this.inProgress = true;
+
+        setTimeout(
+            () => {
+                this.showServerErrors = true;
+                this.inProgress = false;
+            },
+            // tslint:disable-next-line:no-magic-numbers
+            1000
+        );
+    }
+}
