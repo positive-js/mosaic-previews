791ff4baed3f6d1b1ab040076632f6aa76d84985
Mike Ozornin <mike.ozornin@gmail.com>
2021-12-20 14:13:32 +0000

    ffd04245 Checkbox: fix focus state

---
+++ pr748-ffd0424/docs-content/examples-source/mosaic/date-formatter/range-date-formatter/range-date-formatter-example.ts
@@ -0,0 +1,240 @@
+/* tslint:disable:no-magic-numbers */
+import { Component } from '@angular/core';
+import { DateAdapter, MC_DATE_LOCALE } from '@ptsecurity/cdk/datetime';
+import { LuxonDateAdapter } from '@ptsecurity/mosaic-luxon-adapter/adapter';
+import { DateFormatter } from '@ptsecurity/mosaic/core';
+import { DateTime } from 'luxon';
+
+
+/**
+ * @title Basic progress range-date-formatter
+ */
+@Component({
+    selector: 'range-date-formatter-example',
+    templateUrl: 'range-date-formatter-example.html',
+    styleUrls: ['range-date-formatter-example.css'],
+    providers: [
+        { provide: MC_DATE_LOCALE, useValue: 'ru' },
+        { provide: DateAdapter, useClass: LuxonDateAdapter, deps: [MC_DATE_LOCALE] }
+    ]
+})
+export class RangeDateFormatterExample {
+    formats = {
+        ru: {
+            range: {
+                long: {
+                    date: {
+                        currentMonth: '',
+                        notCurrentYear: '',
+                        startsNotCurrentYear: '',
+                        endsNotCurrentYear: ''
+                    },
+                    dateTime: {
+                        startsNotCurrentYear: '',
+                        endsNotCurrentYear: '',
+                        sameDateCurrentYear: '',
+                        sameDateNotCurrentYear: '',
+                        notCurrentMonth: ''
+                    }
+                },
+                middle: {
+                    dateTime: {
+                        currentYear: '',
+                        sameDateCurrentYear: '',
+                        sameDateNotCurrentYear: '',
+                        notCurrentMonth: '',
+                        startsNotCurrentYear: '',
+                        endsNotCurrentYear: ''
+                    }
+                },
+                short: {
+                    date: {
+                        currentMonth: '',
+                        notCurrentYear: '',
+                        startsNotCurrentYear: '',
+                        endsNotCurrentYear: ''
+                    },
+                    dateTime: {
+                        sameDateCurrentYear: '',
+                        sameDateNotCurrentYear: '',
+                        notCurrentMonth: '',
+                        startsNotCurrentYear: '',
+                        endsNotCurrentYear: ''
+                    }
+                }
+            }
+        },
+        en: {
+            range: {
+                long: {
+                    date: {
+                        currentMonth: '',
+                        notCurrentYear: '',
+                        startsNotCurrentYear: '',
+                        endsNotCurrentYear: ''
+                    },
+                    dateTime: {
+                        startsNotCurrentYear: '',
+                        endsNotCurrentYear: '',
+                        sameDateCurrentYear: '',
+                        sameDateNotCurrentYear: '',
+                        notCurrentMonth: ''
+                    }
+                },
+                middle: {
+                    dateTime: {
+                        currentYear: '',
+                        sameDateCurrentYear: '',
+                        sameDateNotCurrentYear: '',
+                        notCurrentMonth: '',
+                        startsNotCurrentYear: '',
+                        endsNotCurrentYear: ''
+                    }
+                },
+                short: {
+                    date: {
+                        currentMonth: '',
+                        notCurrentYear: '',
+                        startsNotCurrentYear: '',
+                        endsNotCurrentYear: ''
+                    },
+                    dateTime: {
+                        sameDateCurrentYear: '',
+                        sameDateNotCurrentYear: '',
+                        notCurrentMonth: '',
+                        startsNotCurrentYear: '',
+                        endsNotCurrentYear: ''
+                    }
+                }
+            }
+        }
+    };
+
+    constructor(private adapter: DateAdapter<DateTime>, private formatter: DateFormatter<DateTime>) {
+        this.populateRangeLong('ru');
+        this.populateRangeLong('en');
+
+        this.populateRangeMiddle('ru');
+        this.populateRangeMiddle('en');
+
+        this.populateRangeShort('ru');
+        this.populateRangeShort('en');
+    }
+
+    private populateRangeShort(locale: string) {
+        this.formatter.setLocale(locale);
+        this.adapter.setLocale(locale);
+
+        const shortRange = this.formats[locale].range.short;
+        const now = this.adapter.today();
+
+        shortRange.date.currentMonth = this.formatter.rangeShortDate(now.set({ day: 1 }), now.set({ day: 10 }));
+        shortRange.date.notCurrentYear = this.formatter.rangeShortDate(
+            now.set({ day: 1, month: 1 }),
+            now.set({ day: 10, month: 2 })
+        );
+        shortRange.date.startsNotCurrentYear = this.formatter.rangeShortDate(
+            now.set({ day: 1, month: 1 }).minus({ years: 1 }),
+            now.set({ day: 10, month: 2 })
+        );
+        shortRange.date.endsNotCurrentYear = this.formatter.rangeShortDate(
+            now.set({ day: 1, month: 1 }),
+            now.set({ day: 10, month: 2 }).plus({ years: 1 })
+        );
+        shortRange.dateTime.sameDateCurrentYear = this.formatter.rangeShortDateTime(
+            now.set({ day: 10, hour: 10, minute: 14 }),
+            now.set({ day: 10, hour: 11, minute: 28 })
+        );
+        shortRange.dateTime.sameDateNotCurrentYear = this.formatter.rangeShortDateTime(
+            now.set({ month: 1, day: 11 }).minus({ years: 1 }).set({ hour: 10, minute: 14 }),
+            now.set({ month: 1, day: 11 }).minus({ years: 1 }).set({ hour: 11, minute: 28 })
+        );
+        shortRange.dateTime.notCurrentMonth = this.formatter.rangeShortDateTime(
+            now.set({ month: 1, day: 1, hour: 10, minute: 14 }),
+            now.set({ month: 2, day: 1, hour: 11, minute: 28 })
+        );
+        shortRange.dateTime.startsNotCurrentYear = this.formatter.rangeShortDateTime(
+            now.set({ month: 1, day: 1 }).minus({ years: 1 }).set({ hour: 10, minute: 14 }),
+            now.set({ month: 2, day: 1, hour: 11, minute: 28 })
+        );
+        shortRange.dateTime.endsNotCurrentYear = this.formatter.rangeShortDateTime(
+            now.set({ month: 1, day: 1, hour: 10, minute: 14 }),
+            now.set({ day: 1, month: 2 }).plus({ years: 1 }).set({ hour: 11, minute: 28 })
+        );
+    }
+
+    private populateRangeMiddle(locale: string) {
+        this.formatter.setLocale(locale);
+        this.adapter.setLocale(locale);
+
+        const middleRange = this.formats[locale].range.middle;
+        const now = this.adapter.today();
+
+        middleRange.dateTime.currentYear = this.formatter.rangeMiddleDateTime(
+            now.set({ day: 1 }),
+            now.set({ day: 10 })
+        );
+        middleRange.dateTime.sameDateCurrentYear = this.formatter.rangeMiddleDateTime(
+            now.set({ day: 10, hour: 10, minute: 14 }),
+            now.set({ day: 10, hour: 10, minute: 28 })
+        );
+        middleRange.dateTime.sameDateNotCurrentYear = this.formatter.rangeMiddleDateTime(
+            now.set({ month: 1, day: 11 }).minus({ years: 1 }).set({ hour: 10, minute: 14 }),
+            now.set({ month: 1, day: 11 }).minus({ years: 1 }).set({ hour: 11, minute: 28 })
+        );
+        middleRange.dateTime.notCurrentMonth = this.formatter.rangeMiddleDateTime(
+            now.set({ month: 1, day: 1, hour: 10, minute: 14 }),
+            now.set({ month: 2, day: 1, hour: 11, minute: 28 })
+        );
+        middleRange.dateTime.startsNotCurrentYear = this.formatter.rangeMiddleDateTime(
+            now.set({ month: 1, day: 1 }).minus({ years: 1 }).set({ hour: 10, minute: 14 }),
+            now.set({ month: 1, day: 1, hour: 11, minute: 28 })
+        );
+        middleRange.dateTime.endsNotCurrentYear = this.formatter.rangeMiddleDateTime(
+            now.set({ month: 1, day: 1, hour: 10, minute: 14 }),
+            now.set({ month: 1, day: 1 }).plus({ years: 1 }).set({ hour: 11, minute: 28 })
+        );
+    }
+
+    private populateRangeLong(locale: string) {
+        this.formatter.setLocale(locale);
+        this.adapter.setLocale(locale);
+
+        const longRange = this.formats[locale].range.long;
+        const now = this.adapter.today();
+
+        longRange.date.currentMonth = this.formatter.rangeLongDate(now.set({ day: 1 }), now.set({ day: 10 }));
+        longRange.date.notCurrentYear = this.formatter.rangeLongDate(
+            now.set({ month: 1, day: 1 }),
+            now.set({ month: 2, day: 10 })
+        );
+        longRange.date.startsNotCurrentYear = this.formatter.rangeLongDate(
+            now.set({ month: 1, day: 1 }).minus({ years: 1 }),
+            now.set({ month: 2, day: 10 })
+        );
+        longRange.date.endsNotCurrentYear = this.formatter.rangeLongDate(
+            now.set({ month: 1, day: 1 }),
+            now.set({ month: 2, day: 10 }).plus({ years: 1 })
+        );
+        longRange.dateTime.sameDateCurrentYear = this.formatter.rangeLongDateTime(
+            now.set({ day: 10, hour: 10, minute: 14 }),
+            now.set({ day: 10, hour: 11, minute: 28 })
+        );
+        longRange.dateTime.sameDateNotCurrentYear = this.formatter.rangeLongDateTime(
+            now.set({ month: 1, day: 11 }).minus({ years: 1 }).set({ hour: 10, minute: 14 }),
+            now.set({ month: 1, day: 11 }).minus({ years: 1 }).set({ hour: 11, minute: 28 })
+        );
+        longRange.dateTime.notCurrentMonth = this.formatter.rangeLongDateTime(
+            now.set({ month: 1, day: 1, hour: 10, minute: 14 }),
+            now.set({ month: 2, day: 1, hour: 11, minute: 28 })
+        );
+        longRange.dateTime.startsNotCurrentYear = this.formatter.rangeLongDateTime(
+            now.set({ month: 1, day: 1 }).minus({ years: 1 }).set({ hour: 10, minute: 14 }),
+            now.set({ month: 2, day: 1, hour: 11, minute: 28 })
+        );
+        longRange.dateTime.endsNotCurrentYear = this.formatter.rangeLongDateTime(
+            now.set({ month: 1, day: 1, hour: 10, minute: 14 }),
+            now.set({ month: 2, day: 1 }).minus({ years: 1 }).set({ hour: 11, minute: 28 })
+        );
+    }
+}
