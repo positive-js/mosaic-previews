1fdcdf8da7da6aa175dacc9822b7c2ab3bbbd841
Mike Ozornin <mike.ozornin@gmail.com>
2022-01-20 20:36:01 +0000

    e3cf7291 Almost fake commit to trigger build after master rebase

---
+++ pr791-e3cf729/docs-content/examples-source/mosaic/modal/modal-component/modal-component-example.ts
@@ -0,0 +1,77 @@
+// tslint:disable:no-console
+import { Component, Input, ViewChild } from '@angular/core';
+import { McButton } from '@ptsecurity/mosaic/button';
+import { ThemePalette } from '@ptsecurity/mosaic/core';
+import { McModalRef, McModalService } from '@ptsecurity/mosaic/modal';
+
+
+/**
+ * @title Component Modal
+ */
+@Component({
+    selector: 'modal-component-example',
+    templateUrl: 'modal-component-example.html',
+    styleUrls: ['modal-component-example.css']
+})
+export class ModalComponentExample {
+    componentModal: McModalRef;
+
+    @ViewChild('modalButton') modalButton: McButton;
+
+    constructor(private modalService: McModalService) {}
+
+    openModal() {
+        this.componentModal = this.modalService.open({
+            mcComponent: McModalCustomComponent,
+            mcComponentParams: {
+                title: 'Title',
+                subtitle: 'Subtitle'
+            }
+        });
+
+        this.componentModal.afterOpen.subscribe(() => {
+            console.log('[afterOpen] emitted!');
+        });
+
+        this.componentModal.afterClose.subscribe((action: 'save' | 'close') => {
+            this.modalButton.focusViaKeyboard();
+
+            console.log(`[afterClose] emitted, chosen action: ${action}`);
+        });
+    }
+}
+
+@Component({
+    selector: 'mc-modal-full-custom-component',
+    template: `
+        <mc-modal-title>
+            Modal Title
+        </mc-modal-title>
+
+        <mc-modal-body>
+            <h2>{{ title }}</h2>
+            <h4>{{ subtitle }}</h4>
+            <p>
+                <span>Get Modal instance in component</span>
+                <button mc-button [color]="themePalette.Primary" (click)="destroyModal('close')">destroy modal in the component</button>
+            </p>
+        </mc-modal-body>
+
+        <div mc-modal-footer>
+            <button mc-button [color]="themePalette.Primary" (click)="destroyModal('save')">Save</button>
+            <button mc-button autofocus (click)="destroyModal('close')">Close</button>
+        </div>
+  `
+})
+export class McModalCustomComponent {
+    themePalette = ThemePalette;
+
+    @Input() title: string;
+    @Input() subtitle: string;
+
+    constructor(private modal: McModalRef) { }
+
+    destroyModal(action: 'save' | 'close') {
+        this.modal.destroy(action);
+    }
+}
