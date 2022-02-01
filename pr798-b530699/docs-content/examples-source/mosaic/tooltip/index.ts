ee6d6656d7bc1ab70f280be53a033c9af4e82b43
oburdasov <oburdasov@ptsecurity.com>
2022-02-01 09:45:46 +0000

    b5306998 Fix image tooltip docs

---
+++ pr798-b530699/docs-content/examples-source/mosaic/tooltip/index.ts
@@ -0,0 +1,30 @@
+import { A11yModule } from '@angular/cdk/a11y';
+import { CdkScrollableModule } from '@angular/cdk/scrolling';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { McButtonModule } from '@ptsecurity/mosaic/button';
+import { McToolTipModule } from '@ptsecurity/mosaic/tooltip';
+
+import { TooltipOverviewExample } from './tooltip-overview/tooltip-overview-example';
+
+
+export {
+    TooltipOverviewExample
+};
+
+const EXAMPLES = [
+    TooltipOverviewExample
+];
+
+@NgModule({
+    imports: [
+        CommonModule,
+        A11yModule,
+        McButtonModule,
+        McToolTipModule,
+        CdkScrollableModule
+    ],
+    declarations: EXAMPLES,
+    exports: EXAMPLES
+})
+export class TooltipExamplesModule {}
