c8deaf7f355de1560057c68d6c5207e1483620f3
l.s.kramarov <l.s.kramarov@gmail.com>
2022-02-11 07:42:40 +0000

    5877a666 feat(docs): add multiple lines and placement examples (#UIM-864)

---
+++ pr798-5877a66/docs-content/examples-source/mosaic/popover/index.ts
@@ -0,0 +1,45 @@
+import { A11yModule } from '@angular/cdk/a11y';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { McButtonModule } from '@ptsecurity/mosaic/button';
+import { McCheckboxModule } from '@ptsecurity/mosaic/checkbox';
+import { McFormsModule } from '@ptsecurity/mosaic/core';
+import { McFormFieldModule } from '@ptsecurity/mosaic/form-field';
+import { McIconModule } from '@ptsecurity/mosaic/icon';
+import { McInputModule } from '@ptsecurity/mosaic/input';
+import { McPopoverModule } from '@ptsecurity/mosaic/popover';
+import { McSelectModule } from '@ptsecurity/mosaic/select';
+
+import { PopoverInstanceExample } from './popover-instance/popover-instance-example';
+import { PopoverOverviewExample } from './popover-overview/popover-overview-example';
+
+
+export {
+    PopoverOverviewExample,
+    PopoverInstanceExample
+};
+
+const EXAMPLES = [
+    PopoverOverviewExample,
+    PopoverInstanceExample
+];
+
+@NgModule({
+    imports: [
+        CommonModule,
+        A11yModule,
+        FormsModule,
+        McFormsModule,
+        McFormFieldModule,
+        McSelectModule,
+        McPopoverModule,
+        McButtonModule,
+        McIconModule,
+        McInputModule,
+        McCheckboxModule
+    ],
+    declarations: EXAMPLES,
+    exports: EXAMPLES
+})
+export class PopoverExamplesModule {}
