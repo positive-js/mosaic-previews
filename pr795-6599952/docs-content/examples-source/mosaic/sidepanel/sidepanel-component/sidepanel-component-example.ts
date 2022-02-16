5fcf0c9b2f5be6f70fc66af617c6e783e8ac65c2
l.s.kramarov <l.s.kramarov@gmail.com>
2022-02-16 10:45:37 +0000

    65999529 fix: исправления техрайтеров

---
+++ pr795-6599952/docs-content/examples-source/mosaic/sidepanel/sidepanel-component/sidepanel-component-example.ts
@@ -0,0 +1,73 @@
+import { Component, Inject, ViewEncapsulation } from '@angular/core';
+import { ThemePalette } from '@ptsecurity/mosaic/core';
+import { MC_SIDEPANEL_DATA, McSidepanelPosition, McSidepanelService } from '@ptsecurity/mosaic/sidepanel';
+
+
+/**
+ * @title Template Modal
+ */
+@Component({
+    selector: 'sidepanel-component-example',
+    templateUrl: 'sidepanel-component-example.html',
+    styleUrls: ['sidepanel-component-example.css'],
+    encapsulation: ViewEncapsulation.None
+})
+export class SidepanelComponentExample {
+    themePalette = ThemePalette;
+
+    position: McSidepanelPosition = McSidepanelPosition.Right;
+
+    modalState: boolean = false;
+
+    constructor(private sidepanelService: McSidepanelService) {}
+
+    openComponentSidepanel() {
+        this.sidepanelService.open(SidepanelExampleCustomComponent, {
+            position: this.position,
+            hasBackdrop: this.modalState,
+            data: {
+                openComponentSidepanel: () => { this.openComponentSidepanel(); }
+            }
+        });
+    }
+}
+
+@Component({
+    selector: 'example-sidepanel',
+    template: `
+    <mc-sidepanel-header [closeable]="true">
+        Sidepanel Component Content
+    </mc-sidepanel-header>
+    <mc-sidepanel-body class="layout-padding">
+        <div class="mc-subheading">Sidepanel Component Body</div>
+
+        <div *ngFor="let item of array; index as i">
+            {{ i + 1 }}
+        </div>
+    </mc-sidepanel-body>
+    <mc-sidepanel-footer>
+        <mc-sidepanel-actions align="right">
+            <button cdkFocusInitial mc-button [color]="themePalette.Primary" (click)="openComponentSidepanel()">
+                <span>Open another sidepanel</span>
+            </button>
+
+            <button mc-button [color]="themePalette.Second" mc-sidepanel-close>
+                <span>Close</span>
+            </button>
+        </mc-sidepanel-actions>
+    </mc-sidepanel-footer>`,
+    host: {
+        class: 'layout-column flex sidepanel-container'
+    }
+})
+export class SidepanelExampleCustomComponent {
+    themePalette = ThemePalette;
+
+    openComponentSidepanel: () => void;
+
+    array = new Array(60); // tslint:disable-line
+
+    constructor(@Inject(MC_SIDEPANEL_DATA) public data: any) {
+        this.openComponentSidepanel = data.openComponentSidepanel;
+    }
+}
