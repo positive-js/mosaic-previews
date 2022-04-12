27855e5d3827330531cd99a63d721ac12e247c0f
Igor Tsaplev <itsaplev@gmail.com>
2022-04-12 14:34:16 +0000

    6df97f1f fix(tree): build fixes (UIM-637)

---
+++ pr852-6df97f1/docs-content/examples-source/mosaic/navbar/index.ts
@@ -0,0 +1,37 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { McButtonModule } from '@ptsecurity/mosaic/button';
+import { McDropdownModule } from '@ptsecurity/mosaic/dropdown';
+import { McIconModule } from '@ptsecurity/mosaic/icon';
+import { McNavbarModule } from '@ptsecurity/mosaic/navbar';
+import { McPopoverModule } from '@ptsecurity/mosaic/popover';
+import { McToolTipModule } from '@ptsecurity/mosaic/tooltip';
+
+import { NavbarOverviewExample } from './navbar-overview/navbar-overview-example';
+import { NavbarVerticalExample } from './navbar-vertical/navbar-vertical-example';
+
+
+export {
+    NavbarOverviewExample,
+    NavbarVerticalExample
+};
+
+const EXAMPLES = [
+    NavbarOverviewExample,
+    NavbarVerticalExample
+];
+
+@NgModule({
+    imports: [
+        CommonModule,
+        McNavbarModule,
+        McIconModule,
+        McButtonModule,
+        McDropdownModule,
+        McPopoverModule,
+        McToolTipModule
+    ],
+    declarations: EXAMPLES,
+    exports: EXAMPLES
+})
+export class NavbarExamplesModule {}
